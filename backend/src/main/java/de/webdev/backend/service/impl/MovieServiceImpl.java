424fb2617d8e72b2e5fadf832dcae9d9e6f16c39
Pourya <pourya.nejadensan@gmail.com>
2024-08-14 15:13:40 +0200

    impel getAllMovies with unit test.

--- backend/src/main/java/de/webdev/backend/service/impl/MovieServiceImpl.java
+++ backend/src/main/java/de/webdev/backend/service/impl/MovieServiceImpl.java
@@ -1,15 +1,21 @@
 package de.webdev.backend.service.impl;
 
 import de.webdev.backend.model.Movie;
+import de.webdev.backend.repository.MovieRepository;
 import de.webdev.backend.service.MovieService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MovieServiceImpl implements MovieService {
+
+    private final MovieRepository movieRepository;
+
     @Override
     public List<Movie> getAllMovies() {
-        return List.of();
+        return movieRepository.findAll();
     }
 }
---
+++ backend/src/test/java/de/webdev/backend/service/impl/MovieServiceImplTest.java
@@ -0,0 +1,34 @@
+package de.webdev.backend.service.impl;
+
+import de.webdev.backend.model.Movie;
+import de.webdev.backend.repository.MovieRepository;
+import de.webdev.backend.service.MovieService;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class MovieServiceImplTest {
+
+    private final MovieRepository movieRepository = mock(MovieRepository.class);
+    private final MovieService movieService = new MovieServiceImpl(movieRepository);
+
+    @Test
+    void getAllMovies() {
+        //GIVEN
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("1","First movie", "First author"));
+        movies.add(new Movie("2","Second movie", "Second author"));
+        when(movieRepository.findAll()).thenReturn(movies);
+
+        //WHEN
+        List<Movie> actual = movieService.getAllMovies();
+
+        //THEN
+        assertEquals(movies, actual);
+        verify(movieRepository).findAll();
+    }
+}
