611a28faa5c6d221522909f6550b4db61ffe6820
henriquemarques <henriquedms7@gmail.com>
2024-08-16 11:40:01 +0200

    add integration test

--- backend/src/test/java/de/webdev/backend/controller/MovieControllerTest.java
+++ backend/src/test/java/de/webdev/backend/controller/MovieControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,9 +20,36 @@ class MovieControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @DirtiesContext
     void getMovie_shouldRetunEmptyList_whenCallInitially() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/movies"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void postMovie_shouldReturnANewMovie() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/movies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+
+                        {
+                        "author": "exampleAuthor",
+                        "title": "exampleTitle"
+                        }
+                        """
+
+                ))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                        "author": "exampleAuthor",
+                        "title": "exampleTitle"
+                        }
+"""
+                ))
+                .andExpect(jsonPath("$.id").exists());
     }
 }
