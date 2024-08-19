4de00239ecb4f305934659736a7eb2d58be31560
Fabian Döz <fabian@doez.info>
2024-08-19 15:04:37 +0200

    adding tests for getById request

--- backend/src/main/java/de/webdev/backend/controller/MovieController.java
+++ backend/src/main/java/de/webdev/backend/controller/MovieController.java
@@ -3,6 +3,7 @@ package de.webdev.backend.controller;
 import de.webdev.backend.model.Movie;
 import de.webdev.backend.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,11 @@ public class MovieController {
     }
 
     @GetMapping("{id}")
-    public Movie getMovieById(@PathVariable String id) {
-        return movieService.getMovieById(id);
+    public ResponseEntity<Movie> getMovieById(@PathVariable String id) {
+        Movie movie= movieService.getMovieById(id);
+        if (movie == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movie);
     }
 }
--- backend/src/test/java/de/webdev/backend/controller/MovieControllerTest.java
+++ backend/src/test/java/de/webdev/backend/controller/MovieControllerTest.java
@@ -1,12 +1,18 @@
 package de.webdev.backend.controller;
 
+import de.webdev.backend.model.Movie;
+import de.webdev.backend.service.MovieService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,10 +21,30 @@ class MovieControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private MovieService movieService;
+
     @Test
     void getMovie_shouldRetunEmptyList_whenCallInitially() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/movies"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("[]"));
+    }
+
+    @Test
+    void getMovieById_shouldReturnMovie_whenIdExists() throws Exception {
+        Movie movie = new Movie("1", "Title", "Author");
+        when(movieService.getMovieById("1")).thenReturn(movie);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/movies/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"id\":\"1\",\"title\":\"Title\",\"author\":\"Author\"}"));
+    }
+
+    @Test
+    void getMovieById_shouldReturnNotFound_whenIdDoesNotExist() throws Exception {
+        when(movieService.getMovieById("999")).thenReturn(null);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/movies/999"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
