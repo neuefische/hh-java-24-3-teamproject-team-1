2a5d4266cf2ed4a3b9fe0ddf904787f6748e518a
Pourya <pourya.nejadensan@gmail.com>
2024-08-21 17:05:07 +0200

    Add Edit (PUT) movie.

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,30 +1,34 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {Movie} from "../models/movie.tsx";
+import EditMovieForm from "./EditMovieForm.tsx";
 
 export default function MovieCard() {
     const {id} = useParams();
-    const [movie, setMovie] = useState({} as Movie);
-  async function fetchMovie() {
-      try {
-          const response = await axios.get(`/api/movies/${id}`);
-          setMovie(response.data);
-      } catch (error) {
-          console.error('Error fetching data:', error);
-      }
-  }
+    const [movie, setMovie] = useState();
+    async function fetchMovie() {
+        try {
+            const response = await axios.get(`/api/movies/${id}`);
+            setMovie(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
 
     useEffect(() => {
         fetchMovie();
     }, [id]);
-console.log(movie);
+
+    if (!movie) return (<p>Is loading...!</p>);
+
     return (
-        <div>
-            <h2>Movie Title: {movie.title}</h2>
-            <p>Author: {movie.author}</p>
-           {/* <p>Genre: {movie.genre}</p>
-            <p>Date of publication: {movie.date}</p>*/}
-        </div>
-    )
+            <div>
+                <h2>Movie Title: {movie.title}</h2>
+                <p>Author: {movie.author}</p>
+                <p>Genre: {movie.genre}</p>
+                <p>Date of publication: {movie.publicationDate.toString()}</p>
+
+                <EditMovieForm defaultMovie={ movie }/>
+            </div>
+        )
 }
